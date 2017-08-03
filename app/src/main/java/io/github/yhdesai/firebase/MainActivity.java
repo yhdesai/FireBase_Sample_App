f268293f52473baf2022435f99a9dfbd87f1e474
Yhdesai <yhdesai99@ymail.com>
2017-08-04 01:56:47 +0530

    Initial commit

---
+++ app/src/main/java/io/github/yhdesai/firebase/MainActivity.java
@@ -0,0 +1,20 @@
+package io.github.yhdesai.firebase;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Admob (View view) {
+        Intent intent = new Intent(MainActivity.this, admob.class);
+        startActivity(intent);}
+
+}
